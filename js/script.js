34e0f117a29af517a29df17d7b2ff6a1a53113d9
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-10-04 14:45:28 -0400

    Done

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Ashley Monaghan All rights reserved
+//
+// Created by: Ashley Monaghan
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+
+ function myButtonClicked() {
+    document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  }
